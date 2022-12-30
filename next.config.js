d1d6e66d95bf066d630e6ace5175c30bc17ea408
Esteban Amazo <bymzno@gmail.com>
2022-12-30 12:00:01 -0500

    change next Config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,12 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['app-ambly.s3.amazonaws.com'],
+    unoptimized: true
   },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  
 };
