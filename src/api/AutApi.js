4db8319bc82c1ce9eafbccb5ec2c2aeb204244fe
Esteban Benjumea <benjumeac12@gmail.com>
2022-12-30 13:24:50 -0500

    delete comments

--- src/api/AutApi.js
+++ src/api/AutApi.js
@@ -4,7 +4,7 @@ import { api_url, headers} from "./config";
 export class AuthApi {
   async UserLogin(data) {
     try {
-      return await axios.post(`${api_url}/auth/login`, data, config);
+      return await axios.post(`${api_url}/auth/login`, data);
     } catch (error) {
       return error.response;
     }
